f09d5f2fcda4fb0bc1281b9be9b4d017f4c526aa
akashkumar62 <xiome555@gmail.com>
2025-10-08 16:15:36 +0530

    cors updated

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'twittervideodownloader-gilt.vercel.app'],
+  methods: ['GET','POST','OPTIONS'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Rate limiting
